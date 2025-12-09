bfc013eed2a7acd07068926f472e6c129153b196
drex-cloud <dandantez168@gmail.com>
2025-12-09 08:34:34 +0300

    Allow Netlify connection

--- notesapi/settings.py
+++ notesapi/settings.py
@@ -148,12 +148,7 @@ SIMPLE_JWT = {
 # ----------------------------
 # CORS Settings
 # ----------------------------
-CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:5500",
-    "http://localhost:5500",
-    "http://127.0.0.1:5501",
-    
-]
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
 
 # ----------------------------
